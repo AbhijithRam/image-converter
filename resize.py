93c8effb292a01bee036949a8290488a636bfb8e
AbhijithRam <abhijithram@ug.cusat.ac.in>
2022-03-09 23:41:56 +0530

    initial commit

---
+++ resize.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import sys
+
+try:
+    fullimageFileName = sys.argv[1]
+
+    operation = sys.argv[2]
+
+    argument = sys.argv[3]
+
+    if operation == '-f':
+        
+        r = fullimageFileName.split('.')
+
+        imageFileName = r[0]
+
+        im = Image.open(fullimageFileName)
+
+        converted = im.convert('RGB')
+
+        converted.save(f'{imageFileName}t.{argument}')
+        converted.show()
+
+
+    elif operation == '-r':
+        im = Image.open(fullimageFileName)
+        r = argument.split('x')
+        r_tuple = (int(r[0]), int(r[1]))
+        out = im.resize(r_tuple)
+        out.save(f'output_{fullimageFileName}')
+        out.show()
+
+except:
+    print('Usage of converter')
+    print('command:')
+    print('python resize.py [input file name] [operation] [argument]')
+    print('Operation to be carried: ')
+    print('-f : format image')
+    print('Example:')
+    print('1: python resize.py image.jpeg -f png ')
+    print('2: python resize.py image.png -f jpeg ')
+    print()
+    print('-r :To resize image')
+    print('Example:')
+    print('1: python resize.py image.jpeg -r 200x200 ')
+    print('2: python resize.py image.jpeg -r 250x200 ')
